2170f2bed729550bb6f850c3c5afaa953e651edc
Michael Gerver <mgerver@ufl.edu>
2020-11-04 14:55:03 -0500

    Add hamburger menu and fix logo heights

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Hamburger Menu Code https://codepen.io/Tetsu4819/pen/dyYwRve
+ham.addEventListener('click', function() {
+	ham.classList.toggle('clicked');
+  menu_wrapper.classList.toggle('clicked');
+});
